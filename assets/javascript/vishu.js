fda998d43728b25ed18ef4ed511b6c55b3270938
Enragedtoast <edwardsjtimothy@gmail.com>
2019-10-11 17:03:15 -0500

    vishu color btns working

--- assets/javascript/vishu.js
+++ assets/javascript/vishu.js
@@ -1,23 +1,29 @@
 $(()=> {
+
+    //vishu wisdom
     const theBookOfVishu = ["#devlyfe", "That's a div.", "All of you look into my eyes: you can do it.", "Duuuuuude", "Are you debugging? You should be debugging.", "Have a cookie!", "Take a break and breathe for 20 seconds...", "Stand up and stretch!", "Claps!" ];
 
     
+    //wisdom on page load
     let verseOfVishu = Math.floor(Math.random() * theBookOfVishu.length);
     let vishuSays = theBookOfVishu[verseOfVishu];
 
     $("#the-word-of-vishu").html(vishuSays);
     
-    
+    //click on vishu for new wisdom
     $("#real-vishu").click(()=> {
         let verseOfVishu = Math.floor(Math.random() * theBookOfVishu.length);
         let vishuSays = theBookOfVishu[verseOfVishu];
         $("#the-word-of-vishu").html("").html(vishuSays);
     });
 
+    //give vishu one of three exciting background colors
+    $(".vishu-color").click(function(){
+        let vishuColor = $(this).data("color")
+        $("body, html").css("background-color", vishuColor)
+    })
 
 
-
-    
 
 
 
